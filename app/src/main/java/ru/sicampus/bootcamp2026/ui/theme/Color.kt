52328229d569f4aec30ce0fb235088b4c51e96c9
Ba1garka <NadiaDi6268@gmail.com>
2026-02-01 04:27:50 +0300

    feat: Макеты основных экранов

--- app/src/main/java/ru/sicampus/bootcamp2026/ui/theme/Color.kt
+++ app/src/main/java/ru/sicampus/bootcamp2026/ui/theme/Color.kt
@@ -8,4 +8,17 @@ val Pink80 = Color(0xFFEFB8C8)
 
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+
+val Pink40 = Color(0xFFEFB8C8)
+
+
+
+
+val Black = Color(0xFF000000)
+
+val BlackIcon = Color(0x80000000)
+val BluePrimary = Color(0xFFBFC8FF)
+
+val SineyIney = Color(0xFF6578F7)
+val Yellow = Color(0xFFFFCA1A)
+val Grey = Color(0x33D9D9D9)
